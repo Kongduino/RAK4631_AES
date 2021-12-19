6095ecb126d1ae7d49b3c8ac76ae68a846793863
Kongduino <dbvic@me.com>
2021-12-19 13:51:36 +0800

    Bug fix for blocks that were not multiples of 16

--- RAK4631_AES.ino
+++ RAK4631_AES.ino
@@ -1,5 +1,5 @@
-#include "Adafruit_nRFCrypto.h"
 #include <Adafruit_TinyUSB.h> // for Serial
+#include "Adafruit_nRFCrypto.h"
 
 void hexDump(char *buf, int len) {
   char alphabet[17] = "0123456789abcdef";
@@ -48,6 +48,10 @@ void testAES() {
   uint8_t myIV[16] =  {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f};
   uint8_t pKey[16] = {0xC6, 0xBB, 0x2D, 0x94, 0x90, 0xB9, 0x65, 0x23, 0x98, 0xED, 0x83, 0x3F, 0x9B, 0x9D, 0x02, 0xCC};
   char pDataIn[65] = "Ca va chez vous les enfants...? Sinon il faut me le dire, hein ?";
+  char smallData[24] = {
+    0xb8, 0x4d, 0x39, 0x1a, 0x03, 0x3a, 0x0f, 0xed, 0x82, 0x97, 0x0d, 0x05,
+    0xb8, 0x4d, 0x39, 0x1a, 0x03, 0x3a, 0x0f, 0xed, 0x82, 0x97, 0x0d, 0x05
+  };
   char pDataOut[65] = {0};
   char pDataRe[65] = {0};
   size_t dataOutBuffSize;
@@ -69,6 +73,27 @@ void testAES() {
   } else {
     hexDump(pDataRe, 64);
   }
+
+  Serial.println(" * Small Blocks");
+  Serial.println("----------------------");
+  memset(pDataOut, 0, 65);
+  memset(pDataRe, 0, 65);
+  hexDump(smallData, 12);
+  err = aes.Process(smallData, 12, myIV, pKey, 16, pDataOut, aes.encryptFlag, aes.ecbMode);
+  // myIV is not needed since it's ECB, but we need the parameter.
+  // Could pass just about anything
+  if (err < 0) {
+    Serial.println("Error " + String(err));
+  } else {
+    hexDump(pDataOut, 16);
+  }
+  err = aes.Process(pDataOut, 16, myIV, pKey, 16, pDataRe, aes.decryptFlag, aes.ecbMode);
+  if (err < 0) {
+    Serial.println("Error " + String(err));
+  } else {
+    hexDump(pDataRe, 12);
+  }
+
   Serial.println("======================");
   Serial.println(" * CBC");
   Serial.println("======================");
